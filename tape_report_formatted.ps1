18fd722eedecc79e4741b7b99001c2e99e000c7c
blindsamurai93 <40183851+blindsamurai93@users.noreply.github.com>
2024-03-20 15:30:48 -0400

    Update tape_report_formatted.ps1

--- tape_report_formatted.ps1
+++ tape_report_formatted.ps1
@@ -5,7 +5,7 @@ $date = Get-Date -Format MM.dd.yyyy
 $Jobs = Get-BEStorage|where name -clike 'T*'| Get-BEJobHistory -FromLastJobRun -JobStatus Succeeded 
 $CompleteRE = [regex] "(Job ended: [a-zA-Z]{0,9}\, [a-zA-Z]{0,9} \d{0,2}\, \d\d\d\d (at) \d{0,2}\:\d\d\:\d\d (AM|PM))"
 
-$dahbool = foreach($_ in $Jobs){
+$finalRun = foreach($_ in $Jobs){
 
  #Converts each line in a job log from the job history list captured in the $Jobs variable to objects
  $backups = $_ |Get-BEJobLog |ConvertFrom-String -delimiter ([environment]::NewLine) 
@@ -13,22 +13,22 @@ $dahbool = foreach($_ in $Jobs){
  #Searches each enumerated log for a pattern matching "Media Label: 000XXXL6"
  $test = $backups | select-string -Pattern "\w*Media Label: \d+\w\d" -AllMatches |Sort-Object |get-unique
  $run = $backups |select-string -Pattern $CompleteRE |sort |Get-Unique
- #Write to host the name of the job and its associated media label w/ job start date (EX. DCLS-OOSNAS-Fern_Epi-Full contains Media Label: 000145L6. Job started: Monday, January 1, 20XX) 
- Write-Output "$($_.name) contains: "  |SORT|Get-Unique |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ #Write to host the name of the job and its associated media label w/ job start date (EX. RUNJOB contains Media Label: 000145L6. Job started: Monday, January 1, 20XX) 
+ Write-Output "$($_.name) contains: "  |SORT|Get-Unique |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  if($test.matches.count -gt 1){
-  write-output "------> $($test.matches.value[0])."|Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
-  write-output "------> $($test.matches.value[1])." |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
-  write-output "------> $($BACKUPS.P5)"  |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
-  write-output "------> $($run.matches.value)" |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
-  write-output "************************"  |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
-  write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($test.matches.value[0])."|Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($test.matches.value[1])." |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($BACKUPS.P5)"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($run.matches.value)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "************************"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  }else{
 
- write-output "------> $($test.matches.value)."  |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
- write-output "------> $($BACKUPS.P5)" |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
- write-output "------> $($run.matches.value)" |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
- write-output "************************"  |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
- write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "\\10.174.125.210\dcls_it_support\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($test.matches.value)."  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($BACKUPS.P5)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($run.matches.value)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "************************"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  }
 
 
@@ -42,7 +42,7 @@ write-host " "
 $Jobs = Get-BEStorage|where name -clike 'T*'| Get-BEJobHistory -FromLastJobRun -JobStatus SucceededWithExceptions 
 $CompleteRE = [regex] "(Job ended: [a-zA-Z]{0,9}\, [a-zA-Z]{0,9} \d{0,2}\, \d\d\d\d (at) \d{0,2}\:\d\d\:\d\d (AM|PM))"
 
-$dahbool = foreach($_ in $Jobs){
+$finalRun = foreach($_ in $Jobs){
 
  #Converts each line in a job log from the job history list captured in the $Jobs variable to objects
  $backups = $_ |Get-BEJobLog |ConvertFrom-String -delimiter ([environment]::NewLine) 
@@ -50,22 +50,22 @@ $dahbool = foreach($_ in $Jobs){
  #Searches each enumerated log for a pattern matching "Media Label: 000XXXL6"
  $test = $backups | select-string -Pattern "\w*Media Label: \d+\w\d" -AllMatches |Sort-Object |get-unique
  $run = $backups |select-string -Pattern $CompleteRE |sort |Get-Unique
- #Write to host the name of the job and its associated media label w/ job start date (EX. DCLS-OOSNAS-Fern_Epi-Full contains Media Label: 000145L6. Job started: Monday, January 1, 20XX) 
- Write-host "$($_.name) contains: "  |SORT|Get-Unique #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
+ #Write to host the name of the job and its associated media label w/ job start date (EX. RUNJOB contains Media Label: 000145L6. Job started: Monday, January 1, 20XX) 
+ Write-host "$($_.name) contains: "  |SORT|Get-Unique #|Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  if($test.matches.count -gt 1){
-  write-host "------> $($test.matches.value[0])."#|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
-  write-host "------> $($test.matches.value[1])." #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
-  write-host "------> $($BACKUPS.P5)"  #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
-  write-host "------> $($run.matches.value)" #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
-  write-host "************************"  #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
-  write-host "\/\/\/\/\/\/\/\/\/\/\/\/" #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($test.matches.value[0])."|Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($test.matches.value[1])." |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($BACKUPS.P5)"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "------> $($run.matches.value)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "************************"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+  write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  }else{
 
- write-host "------> $($test.matches.value)."  #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
- write-host "------> $($BACKUPS.P5)" #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
- write-host "------> $($run.matches.value)" #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
- write-host "************************"  #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
- write-host "\/\/\/\/\/\/\/\/\/\/\/\/" #|Out-File "\\dcls-oosnas\shares\dclsusers\aaxfields\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($test.matches.value)."  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($BACKUPS.P5)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "------> $($run.matches.value)" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "************************"  |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
+ write-output "\/\/\/\/\/\/\/\/\/\/\/\/" |Out-File "[FILEPATH]\BackExecLogs\Tape_Reports\tape_report_$($date).txt" -append 
  }
 }
 
